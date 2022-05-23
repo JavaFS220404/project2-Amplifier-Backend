bb71f1f79ed86a4945f23ff235845d231a11c28a
Tanuja Gowrineni <tgowrineni@gmail.com>
2022-05-23 16:53:03 +0200

    User DAO and Implementation added

--- src/main/java/com/revature/daos/UserDAO.java
+++ src/main/java/com/revature/daos/UserDAO.java
@@ -1,7 +1,13 @@
 package com.revature.daos;
 
+import com.revature.models.User;
+
 public interface UserDAO {
 	
+	public User getByUserName(String username);
+	public void createUser(User user);
+	public void updateUser(User user);
+	public void deleteUser(User user); 
 
 
 }
--- src/main/java/com/revature/daos/UserDAOImpl.java
+++ src/main/java/com/revature/daos/UserDAOImpl.java
@@ -1,6 +1,44 @@
 package com.revature.daos;
 
-public class UserDAOImpl implements UserDAO{
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import com.revature.models.User;
+import com.revature.utils.HibernateUtil;
+
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public User getByUserName(String username) {
+		Session session = HibernateUtil.getSession();
+		User user = session.get(User.class, username);
+		return user;
+	}
+
+	@Override
+	public void createUser(User user) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		session.save(user);
+		tx.commit();
+
+	}
+
+	@Override
+	public void updateUser(User user) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		session.merge(user);
+		tx.commit();
+
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		session.delete(user);
+		tx.commit();
+	}
 
 }
