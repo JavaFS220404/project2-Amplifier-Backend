eacd615ca5ccdc2e5fd4af2da59458876d4244b7
unknown <jawdatfayad@gmail.com>
2022-05-23 16:08:37 +0200

    user is here

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,48 +1,132 @@
 package com.revature.models;
 
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+
 public class User extends AbstractUser {
 
-	private String firstname;
-	private String lastname;
+	@Id
+	private int Id;
+	private String firstName;
+	private String lastName;
 	private String email;
+	private String username;
+	private String password;
+	@ManyToMany
+	private List<Hero> favouriteCharacters;
 
 	public User() {
 		super();
 	}
-
 	
-	public User(int id, String username, String password, Role role) {
-		super(id, username, password, role);
+
+	public User(int id, String firstName, String lastName, String email, String username, String password,
+			List<Hero> favouriteCharacters) {
+		super();
+		Id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.favouriteCharacters = favouriteCharacters;
 	}
 	
-	public User(int id, String username, String password, Role role, String firstname, String lastname, String email ) {
-		super(id, username, password, role);
-		this.firstname = firstname;
-		this.lastname = lastname;
+
+
+	@Override
+	public String toString() {
+		return "User [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", username=" + username + ", password=" + password + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(Id, email, firstName, lastName, password, username);
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Id == other.Id && Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)
+				&& Objects.equals(username, other.username);
+	}
+
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Hero> getFavouriteCharacters() {
+		return favouriteCharacters;
+	}
+
+	public void setFavouriteCharacters(List<Hero> favouriteCharacters) {
+		this.favouriteCharacters = favouriteCharacters;
+	}
 	
-    public String getFirstname() {
-        return firstname;
-    }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-    
-    public String getLastname() {
-        return lastname;
-    }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
