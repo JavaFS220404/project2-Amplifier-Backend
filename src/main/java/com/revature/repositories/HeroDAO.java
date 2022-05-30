71743607787746133bb0cfc7429fc9598d9beee3
SilvanGrosskreutz <silvan.grosskreutz@htp-tel.de>
2022-05-30 15:04:06 +0200

    Clean up

--- src/main/java/com/revature/repositories/HeroDAO.java
+++ src/main/java/com/revature/repositories/HeroDAO.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.models.Hero;
 
-
 public interface HeroDAO extends JpaRepository<Hero, Integer> {
 
 }
--- src/main/java/com/revature/repositories/UserDAO.java
+++ src/main/java/com/revature/repositories/UserDAO.java
@@ -1,10 +1,8 @@
-package com.revature.repositoies;
+package com.revature.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.models.User;
-
-
 
 public interface UserDAO extends JpaRepository<User, Integer>{
 
